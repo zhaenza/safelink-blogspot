77eabe27138620a0424f2ab4201e0ff62e3c32c3
Zaenal Mu <34736669+zhaenza@users.noreply.github.com>
2018-08-19 08:46:53 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="may-coconut-oil-help-psoriasis-symptoms.html"}
-if(x=="2"){xxx="vitamin-supplements-minerals-and-other.html"}
-if(x=="3"){xxx="few-medicare-patients-make-work-with-of.html"}
-if(x=="4"){xxx="cigarette-smokers-better-off-quitting.html"}
-if(x=="5"){xxx="devoid-of-fluoride-toothbrushing-wont.html"}a_to_ve[i].href="https://safelink-laptopdrivers123.blogspot.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/08/may-coconut-oil-help-psoriasis-symptoms.html"}
+if(x=="2"){xxx="/2018/08/vitamin-supplements-minerals-and-other.html"}
+if(x=="3"){xxx="/2018/08/few-medicare-patients-make-work-with-of.html"}
+if(x=="4"){xxx="/2018/08/cigarette-smokers-better-off-quitting.html"}
+if(x=="5"){xxx="/2018/08/devoid-of-fluoride-toothbrushing-wont.html"}a_to_ve[i].href="https://safelink-laptopdrivers123.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
